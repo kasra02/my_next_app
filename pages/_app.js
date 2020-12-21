cb8507a4b8c6694bbe4ab06e55b4345d532b2634
kasra <kasra.niroumand@gmail.com>
2020-12-21 18:22:31 +0330

    adding component and generic header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import Header from '../component/generic/HEADER'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+      <Header/>
+      <Component {...pageProps} />
+  )
 }
 
 export default MyApp
